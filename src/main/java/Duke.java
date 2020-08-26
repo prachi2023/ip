68c9cebde21b6d088f66dbecea6c83b31f749978
prachi2023 <prachi.goenka@u.nus.edu>
2020-08-26 10:32:00 +0800

    add user input, add echoing of commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,18 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner in = new Scanner (System.in);
+
         System.out.println ("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+        String line = in.nextLine();
 
-        System.out.println (" ");
+        while (line.compareTo("bye") != 0 ){
+            System.out.println (line);
+            line = in.nextLine();
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
