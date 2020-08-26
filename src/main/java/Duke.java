e33e504a2e6dffd6e2a0e0743d4c8d568ac23d2b
prachi2023 <prachi.goenka@u.nus.edu>
2020-08-26 10:46:05 +0800

    Add list function. Add display of list of text entered

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,33 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static void printList (String[] list, int listSize){
+        for (int i = 0; i < listSize; i++){
+            System.out.println(i+1 + ". " + list[i]);
+        }
+    }
     public static void main(String[] args) {
         Scanner in = new Scanner (System.in);
-
+        String line;
+        String[] list = new String[100];
+        int listSize = 0;
         System.out.println ("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        String line = in.nextLine();
 
-        while (line.compareTo("bye") != 0 ){
-            System.out.println (line);
+
+        while (true){
             line = in.nextLine();
+            if (line.equals("bye")){
+                break;
+            }
+            if (line.equals("list")) {
+                printList(list, listSize);
+                continue;
+            }
+            System.out.println (line);
+            list[listSize] = line;
+            listSize ++;
+
         }
 
         System.out.println("Bye. Hope to see you again soon!");
