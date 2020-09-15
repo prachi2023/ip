33b86ce45d6104f1f182cc11d72971de17e9cccc
prachi2023 <prachi.goenka@u.nus.edu>
2020-09-16 01:07:44 +0800

    Add method to read and load data already there on the file into duke

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -7,6 +7,7 @@ import duke.task.Event;
 import duke.exception.DukeException;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
@@ -22,15 +23,56 @@ public class Duke {
         File f = new File(path);
         if (!f.exists()){
             f.createNewFile();
+        }else {
+            try{
+                loadData();
+            }catch (FileNotFoundException e){
+                //should not occur since, it has been checked that there is a file
+                System.out.println("File not found");
+            }
         }
-
     }
+    //Load data from file
+    public static void loadData() throws FileNotFoundException {
+        File f = new File(path);
+        Scanner s = new Scanner(f);
+        // get the tasks line by line
+        while (s.hasNext()){
+            // | is the barrier between the information
+            String taskInput = s.nextLine();
+            String[] task = taskInput.split(":");
+
+            System.out.println("");
+            switch (task[0]) {
+            case "T":
+                tasks[numOfTasks] = new ToDo(task[2]);
+                break;
+            case "D":
+                tasks[numOfTasks] = new Deadline(task[2], task[3]);
+                break;
+            case "E":
+                tasks[numOfTasks] = new Event(task[2], task[3]);
+                break;
+            default:
+                tasks[numOfTasks] = new Task(task[2]);
+            }
+            if (task[1] == "true"){
+                tasks[numOfTasks].markTaskDone();
+            }
+            numOfTasks ++;
+        }
+    }
+
     //Add a task to the file
     public static void addTaskToFile (int index) throws IOException{
         FileWriter fw = new FileWriter(path, true);
-        String taskToWrite = tasks[index].toString();
+        String taskToWrite = tasks[index].saveFormat();
         fw.write(taskToWrite + System.lineSeparator());
         fw.close();
+    }
+    //edit the details of the task in the file
+    public static void editTaskInFile (int index) throws IOException{
+
     }
 
     public static void exitDuke (){
@@ -47,8 +89,7 @@ public class Duke {
     }
 
     // Mark a specific task in the list as done
-    public static void markTaskInListAsDone(String index){
-            int taskNumCompleted = Integer.parseInt(index.trim());
+    public static void markTaskInListAsDone(int taskNumCompleted){
 
             if (taskNumCompleted > numOfTasks) {
                 System.out.println("This is not a valid task\nPlease enter a valid task number");
@@ -116,9 +157,13 @@ public class Duke {
             break;
         case "done":
             try {
-                markTaskInListAsDone(userInput[1]);
+                int taskNumCompleted = Integer.parseInt(userInput[1].trim());
+                markTaskInListAsDone(taskNumCompleted);
+                editTaskInFile(taskNumCompleted);
             } catch (NumberFormatException e) {
                 System.out.println("Task index is not a number: please enter a valid integer");
+            } catch (IOException e){
+                System.out.println ("File is unable to be edited");
             }
             break;
         case "todo":
@@ -135,20 +180,26 @@ public class Duke {
             break;
         case "deadline":
             try{
-            addDeadline(userInput[1]);
+                addDeadline(userInput[1]);
+                addTaskToFile (numOfTasks - 1);
             } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("Oopsies, seems like you did not enter a description for the task. A deadline needs a description!");
             } catch (DukeException e){
                 System.out.println("A deadline requires the time it is due");
+            } catch (IOException e){
+                System.out.println ("File is unable to be edited");
             }
             break;
         case "event":
             try{
                 addEvent(userInput[1]);
+                addTaskToFile (numOfTasks - 1);
             } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("Oopsies, seems like you did not enter a description for the task. An event needs a description!");
             } catch (DukeException e){
                 System.out.println("An event requires the time of the event");
+            }catch (IOException e){
+                System.out.println ("File is unable to be edited");
             }
             break;
         // If it is none of the above commands, Tell the user to enter a valid command
