fe2636b12a836d04bbc88d174b4143b3c8309489
PRACHI-ASUS-2\User <prachi.goenka@u.nus.edu>
2020-09-23 13:35:51 +0800

    Edit Duke Exception to initialise with an error message

--- src/main/java/duke/exception/DukeException.java
+++ src/main/java/duke/exception/DukeException.java
@@ -1,5 +1,12 @@
 package duke.exception;
 
 public class DukeException extends Exception {
+    private String errorMessage;
 
+    public DukeException(String errorMessage){
+        this.errorMessage = errorMessage;
+    }
+    public String getErrorMessage(){
+        return errorMessage;
+    }
 }
